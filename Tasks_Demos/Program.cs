50a4fa44b21c6470427f573411d7327a1b7bc251
aec <aec@idm365.com>
2022-04-05 07:01:36 +0200

    Task.WhenAll - Inner exceptions are lost #31494

--- Tasks_Demos/Program.cs
+++ Tasks_Demos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.NetworkInformation;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Tasks_Demos
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             int failed = 0;
             var tasks = new List<Task>();
@@ -55,6 +56,52 @@ namespace Tasks_Demos
                 Console.WriteLine("All ping attempts succeeded.");
             else if (t.Status == TaskStatus.Faulted)
                 Console.WriteLine("{0} ping attempts failed, Message {1}", failed, t.Exception.Message);
+        }
+
+
+        /// <summary>
+        /// Both exceptions are thrown, but Debugger.Break() is hit only once.
+        /// What is more, the exception value is not AggregateException, but InvalidTimeZoneException.
+        /// This is because of new async/await which does the unwrapping into the actual exception.
+        /// If you want to read other Exceptions (not only the first one), you would have to read them from the Task returned from WhenAll method call.
+        /// 
+        /// https://github.com/dotnet/runtime/issues/31494#issuecomment-553941135
+        /// https://stackoverflow.com/questions/49403432/does-task-whenall-wait-for-all-the-tasks-in-case-of-exceptions
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static async Task Main(string[] args)
+        {
+           await TestTaskWhenAll();
+        }
+
+        private static async Task TestTaskWhenAll()
+        {
+            try
+            {
+                await Task.WhenAll(
+                    ShortOperationAsync(),
+                    LongOperationAsync()
+                );
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message); // Short operation exception
+                Debugger.Break();
+            }
+        }
+
+        private static async Task ShortOperationAsync()
+        {
+            await Task.Delay(1000);
+            throw new InvalidTimeZoneException("Short operation exception");
+
+        }
+
+        private static async Task LongOperationAsync()
+        {
+            await Task.Delay(5000);
+            throw new ArgumentException("Long operation exception");
         }
     }
 }
